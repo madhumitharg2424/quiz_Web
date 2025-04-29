553c401a33e50e0f0ca162e33ceccba8eb01d2f2
trickky24 <madhumitha0836@gmail.com>
2025-04-29 08:35:44 +0530

    fdrth

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: './' ,
   plugins: [react()],
   define: {
     __DEFINES__: {}, // ✅ Fix "__DEFINES__ is not defined" error
