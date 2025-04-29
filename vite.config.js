086e27d0d72868e6b7268a1e86a473b38703e534
trickky24 <madhumitha0836@gmail.com>
2025-04-29 08:36:14 +0530

    erd

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: './' ,
+  base: './',
   plugins: [react()],
   define: {
     __DEFINES__: {}, // ✅ Fix "__DEFINES__ is not defined" error
@@ -12,6 +12,13 @@ export default defineConfig({
       scss: {
         additionalData: `@import "src/styles/global.scss";`, // ✅ If using SCSS
       },
+    },
+  },
+  server: {
+    // Add headers for Cross-Origin-Opener-Policy
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
 });
